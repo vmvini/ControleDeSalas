8a2e6aaddf946c14c93cfd744907631b70131181
Marcus Vinícius <vmvini@hotmail.com>
2015-07-07 20:48:40 -0300

    Adicionando exceção UsuarioNaoEncontrado.java ao vmvini.controlesalas.controller

---
+++ src/vmvini/controlesalas/controller/UsuarioNaoEncontrado.java
@@ -0,0 +1,9 @@
+package vmvini.controlesalas.controller;
+
+public class UsuarioNaoEncontrado extends Exception {
+    
+    UsuarioNaoEncontrado(String message){
+        super(message);
+    }
+    
+}
