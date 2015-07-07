0bfdc9ef90b3cc9ec4e6662d8e774dc1c32f794b
Marcus Vinícius <vmvini@hotmail.com>
2015-07-07 20:58:57 -0300

    Adicionando AdminControle.java ao vmvini.controlesalas.controller

---
+++ src/vmvini/controlesalas/controller/AdminControle.java
@@ -0,0 +1,47 @@
+package vmvini.controlesalas.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import vmvini.controlesalas.model.TipoUsuario;
+import vmvini.controlesalas.model.Usuario;
+
+public class AdminControle implements Controle {
+ 
+    ConstrutorDaoInterface construtorDao;
+    UsuarioDAO usuarioDao;
+    
+    public AdminControle() throws SQLException{
+        construtorDao = new ConstrutorDaoDB();
+        usuarioDao =  construtorDao.criarUsuarioDAO(TipoUsuario.ADMIN);
+    }
+    
+    public void cadastrarUsuario(Usuario usuario) throws SQLException{
+        usuarioDao.cadastrarUsuario(usuario);
+    }
+    
+    public void removerUsuario(Usuario usuario) throws OperacaoNaoPermitida, SQLException, UsuarioNuloException{
+        
+        if(usuario.getTipoUsuario().equals(TipoUsuario.ADMIN)){
+            if(!SessaoLogin.getUsuarioLogado().getMatricula().equals(usuario.getMatricula()))
+                throw new OperacaoNaoPermitida("Você não pode removar outro administrador");
+            else{
+                usuarioDao.removerUsuario(usuario);
+                SessaoLogin.logout();
+            }
+        }
+        else
+         usuarioDao.removerUsuario(usuario);
+        
+        
+    }
+    
+    public List<Usuario> listarUsuarios() throws SQLException{
+        return usuarioDao.listarUsuarios();
+    }
+    
+    public Usuario buscarUsuario(String matricula ) throws SQLException, UsuarioNaoEncontrado{
+        return usuarioDao.buscarUsuario(matricula);
+    }
+    
+}
